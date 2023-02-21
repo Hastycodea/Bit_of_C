7ff687234d89b3e9d4799ab06cf276781aa4fe8e
Hastycodea <hastycodeandreh.com>
2023-02-21 12:10:28 +0300

    data types

---
+++ datatypes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	//integers, double, float
+
+	printf("%i %f %f\n", 1, 1.1111, 1111.111110100010001001111111F);
+	return 0;
+}
--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 // single line comments
-
+int add(int a, int b)
+{
+	sum = a + b;
+	
+	return 1;
+}
 /*
 multi
 line 
